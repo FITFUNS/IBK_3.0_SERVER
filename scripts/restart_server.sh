a9233fcdc00ce0f0de310cba5e4ebb17a092132e
kylee <kylee@fitfuns.com>
2025-03-28 16:21:33 +0900

    init ibk

---
+++ scripts/restart_server.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# export PATH=$PATH:/usr/bin:/usr/local/bin:/home/admin/.nvm/versions/node/v20.18.3/bin
+# cd ~/gummyserver.modakgames.com
+sudo systemctl restart main_ibk.service
+echo "done"
+# npm install
+# npm run build
+# pm2 restart destiny
